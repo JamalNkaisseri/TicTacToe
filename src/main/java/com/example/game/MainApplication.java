3935c50904ba4a75ff74fa6ff8b53bb1c8742e0c
JS <jackdesparrow660@gmail.com>
2023-06-28 16:14:46 +0300

    Modify methods

--- src/main/java/com/example/game/MainApplication.java
+++ src/main/java/com/example/game/MainApplication.java
@@ -1,12 +1,7 @@
 package com.example.game;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class MainApplication  {
 
